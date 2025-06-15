2e82c12d9755ffcfffee91cdadf89bf0d62240e2
Orion1024 <orion1024@users.noreply.github.com>
2025-06-15 23:10:01 +0200

    TMP cleanup thread from previous bot messages 4

--- Modules/Discord/bot.py
+++ Modules/Discord/bot.py
@@ -654,7 +654,7 @@ class PiaBot(commands.Bot):
                 thread = message.thread
 
                 # List previous messages from the bot, for later removal if content fetching succeeds
-                previous_bot_messages = [message async for message in thread.history(limit=None) if message.author == self.user and not message.content == strings.DISCORD_THREAD_CREATED]                
+                previous_bot_messages = [message async for message in thread.history(limit=None) if message.author == self.user and not message.content == strings.DISCORD_THREAD_CREATED and not message.content == thread.name]
                 logger.info(f"Bot messages in thread {thread.id} identified for future removal: {len(previous_bot_messages)}")
 
                 await thread.send(strings.CONTENT_FETCHING)
@@ -719,7 +719,7 @@ class PiaBot(commands.Bot):
                     except discord.NotFound:
                      logger.warning(f"Previous bot message not found: {shortened_content}")
                     except discord.Forbidden:
-                        logger.error(f"Failed to delete previous bot message: {shortened_content}")
+                        logger.error(f"Forbidden to delete previous bot message: {shortened_content}")
                     except Exception as e:
                         logger.error(f"Error deleting previous bot message: {shortened_content}, {e}")
             
